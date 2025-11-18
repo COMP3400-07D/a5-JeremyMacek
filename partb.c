ecc7407e7f711a8e317ee37fa3b445c75a6103a4
Jeremy Macek <macekjeremy@kali.os>
2025-11-18 14:10:16 -0500

    Completed Part B

--- partb.c
+++ partb.c
@@ -6,6 +6,23 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+
+    if (argc < 2){
+        printf("ERROR: not enough args");
+        return 1;
+    }
+    
+    int fd = open ("partb_file1_output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    if (fd == -1){
+        perror("Error opening file");
+        return 1;
+    }
+
+    dup2(fd, STDOUT_FILENO);
+    printf("Category,Count\n");
+    fflush(stdout);
+
+    int eret = execv("./parta", argv);
+    
     return 0;
 }
